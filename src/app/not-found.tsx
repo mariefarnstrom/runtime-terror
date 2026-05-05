732ea0ac09e3bf0cc33bb1b1a1b23472df1f2052
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-05 12:33:00 +0200

    Updated not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
     </div>
   );
 }

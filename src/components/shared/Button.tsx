ed0154dd34fda1981c8047570a11762df5ed56d7
Marie Färnström <fernstrm@hotmail.com>
2026-05-07 12:45:03 +0200

    Added DoorButton component and used in all room components, and added backgroudimages to test flow.

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,1 +1,20 @@
-//Example file
+import { useGameStore } from "@/store/useGameStore";
+
+type DoorButtonProps = {
+    buttonText: string;
+}
+
+export default function DoorButton({ buttonText }: DoorButtonProps) {
+    const { /* increaseFear, */ goToNextRoom } = useGameStore();
+
+    const handleDoorOpen = (): void => {
+        /* increaseFear(20); */
+        goToNextRoom();
+    };
+
+    return (
+        <button onClick={handleDoorOpen} className="bg-red-800">
+            {buttonText}
+        </button>
+    );
+}

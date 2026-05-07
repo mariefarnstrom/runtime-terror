2c830aaacef1159088019c19bbd1555ca37aa0d6
Malin Persson <malin.persson02@hotmail.com>
2026-05-07 12:46:02 +0200

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 'use client'
-
 import EnterForm from "@/components/home-page/enter-form";
 import Fog from "@/components/effects/Fog";
 import { ApiError } from "@/types/errors";
 import { useState } from "react";
 import { processPayment } from "@/lib/payment";
+import Link from "next/link";
 
 export default function Home() {
   const [error, setError] = useState<ApiError | null>(null)
+  const ENTRY_PRICE = Number(process.env.NEXT_PUBLIC_ENTRY_PRICE) || 3;
+
 
   const handlePayment = async (name: string) => {
     setError(null)
 
     const result = await processPayment({
-      seller: process.env.SELLER || 'default-seller',
+      seller: process.env.NEXT_PUBLIC_SELLER || 'default-seller',
       buyer: name,
-      amount: 100,
+      amount: ENTRY_PRICE,
     })
 
     if (!result.success) {
@@ -32,12 +34,14 @@ export default function Home() {
       <Fog />
 
       {/* Content — top layer */}
-      <div className="relative z-10 flex flex-col gap-6 w-full h-full items-center">
+      <div className="relative z-10 flex flex-col gap-8 w-full h-full items-center">
         <h1 className="text-red-800 text-5xl leading-normal">Runtime terror</h1>
+        <h3 className="text-white text-2xl">Enter the house for {ENTRY_PRICE}€</h3>
         <div className="w-80vw max-w-80 justify-end">
           <EnterForm onSubmit={handlePayment} />
           {error && <p className="text-red-500 mt-4">Error: {error.message}</p>}
         </div>
+        <Link href="/haunted-house" className="text-white underline mt-4">Enter house (for testing)</Link>
       </div>
     </div>
   );
--- src/components/home-page/enter-form.tsx
+++ src/components/home-page/enter-form.tsx
@@ -13,16 +13,17 @@ export default function EnterForm({ onSubmit }: EnterFormProps) {
         const name = formData.get("name") as string;
         onSubmit(name);
       }}
-      className="bg-gray-500 bg-opacity-50 p-6 rounded shadow-md flex flex-col items-center w-full"
+
+      className="bg-gray-300/60 p-6 rounded shadow-md flex flex-col items-center w-full"
     >
       <label htmlFor="name"
-      className="text-lg"
-      >Name</label>
+      className="text-lg mb-2"
+      >Enter your name</label>
       <input id="name" type="text" required name="name"
       className="border border-black rounded px-2 py-1 mb-4 focus:ring-2 focus:ring-red-700 focus:outline-none transition"
       />
       <button type="submit"
-      className="border border-black rounded px-4 py-2 hover:bg-red-700 hover:text-white transition"
+      className="border border-black rounded px-4 py-2 cursor-pointer hover:bg-red-700 hover:text-white transition focus:ring-2 focus:ring-red-700 focus:outline-none"
       >Enter house</button>
     </form>
   );

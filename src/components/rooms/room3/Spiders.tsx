44a11de0ff8a218c666567e8e738c5aa0766d1a9
Malin Persson <malin.persson02@hotmail.com>
2026-05-07 12:52:17 +0200

    Change background image from JPG to PNG

--- src/components/rooms/room3/Spiders.tsx
+++ src/components/rooms/room3/Spiders.tsx
@@ -3,7 +3,7 @@ import DoorButton from "@/components/shared/Button";
 export default function Spiders() {
 
   return (
-    <div className="absolute inset-0 bg-[url('/assets/images/eerie-hospital.jpg')] bg-cover">
+    <div className="absolute inset-0 bg-[url('/assets/images/eerie-hospital.png')] bg-cover">
       <DoorButton buttonText="Do you dare?" />
     </div>
   );

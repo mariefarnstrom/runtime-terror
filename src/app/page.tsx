3478c207fe9a2d3c40bff7b1df90569e9af40c67
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-08 11:35:37 +0200

    Added color and font variables in globals, and updated the font on homepahe title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,18 @@ export default function Home() {
 
       {/* Content — top layer */}
       <div className="relative z-10 flex flex-col w-full items-center">
-        <h1 className="text-red-800 text-5xl leading-normal">Runtime terror</h1>
+        <h1 className="font-eater text-red-800 text-5xl leading-normal">
+          Runtime terror
+        </h1>
+
+        <div className="bg-black(opacity-80)">
+          <h2 className="font-glitch">
+            Are you a scaredy cat — or do you laugh in the face of horror?
+          </h2>
+          <h2 className="font-glitch">
+            Enter Runtime Terror and find out if you can handle what's inside.
+          </h2>
+        </div>
         <EnterForm />
       </div>
     </div>

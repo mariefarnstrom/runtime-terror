e573926d484cc5b348ef4943b4928b51a2191b10
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-11 12:12:36 +0200

    Changed graveyard image

--- src/components/rooms/room1/Graveyard.tsx
+++ src/components/rooms/room1/Graveyard.tsx
@@ -3,9 +3,8 @@
 import DoorButton from "@/components/shared/Button";
 
 export default function Graveyard() {
-
   return (
-    <div className="absolute inset-0 bg-[url('/assets/images/graveyard.png')] bg-cover">
+    <div className="absolute inset-0 bg-[url('/assets/images/graveyard-night.png')] bg-cover bg-top-left">
       <DoorButton buttonText="Enter the house" />
     </div>
   );

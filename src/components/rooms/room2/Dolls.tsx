efc7273acbc16015f427b31c0f1c03622c1b3a35
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-15 21:34:45 +0200

    Added random doll-laugh as a room-audio in dolls-room

--- src/components/rooms/room2/Dolls.tsx
+++ src/components/rooms/room2/Dolls.tsx
@@ -1,38 +1,39 @@
-import DoorButton from "@/components/shared/DoorTransition";
-import { useEffectSounds } from "@/hooks/useEffectSounds";
 import RockingChair from "@/components/effects/RockingChair";
 import DoorTransition from "@/components/shared/DoorTransition";
+import { useEffect } from "react";
+import { useAudioStore } from "@/store/useAudioStore";
+import { useGameStore } from "@/store/useGameStore";
 
 export default function Dolls() {
   //Example of how to use the useEffectSounds hook
-  const trigger = useEffectSounds({ effect: "jumpscare-piano" });
+  /* const trigger = useEffectSounds({ effect: "jumpscare-piano" }); */
+  const { play } = useAudioStore();
+  const currentRoom = useGameStore((s) => s.currentRoom);
+
+  useEffect(() => {
+    if (currentRoom !== "dolls") return;
+
+    const interval = setInterval(() => {
+      const shouldPlay = Math.random() < 0.4;
+
+      if (shouldPlay) {
+        play("doll-laugh");
+      }
+    }, 15000);
+
+    return () => clearInterval(interval);
+  }, [currentRoom, play]);
 
   return (
     <div
       className="absolute inset-0 bg-cover bg-center bg-no-repeat
       bg-[url('/assets/images/dolls-bg.jpg')]"
     >
-      {/*    <div className="absolute bottom-40 right-140">
-        <img src="/assets/images/dolls2.png" alt="" width={200} height={200} />
-      </div> */}
-      {/* <div className="absolute bottom-35 right-60">
-        <img src="/assets/images/doll-head.png" alt="" width={60} height={60} />
-      </div> */}
-      {/* <div className="absolute bottom-25 left-25">
-        <img src="/assets/images/dolls3.png" alt="" width={200} height={200} />
-      </div> */}
       <div className="absolute inset-0 bg-black/20 pointer-events-none" />
-      {/* <DoorButton buttonText="Enter at own risk" /> */}
-      {/* Example of using the useEffectSounds hook to trigger a sound effect on mouseover */}
-      {/*   <div
-        className="bg-red-600/70 p-4 rounded absolute bottom-20 left-1/2 transform -translate-x-1/2 cursor-pointer"
-        onMouseEnter={trigger}
-      >
-        Mouseover me for a jumpscare sound effect
-      </div> */}
+
       <RockingChair />
-      {/*  <DollsMove /> */}
-      // Spiders — cellar door
+
+      {/* Spiders — cellar door */}
       <DoorTransition
         buttonText="Go further"
         doorImage="/assets/images/cellar-door.png"

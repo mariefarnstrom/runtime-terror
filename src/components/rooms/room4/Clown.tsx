89c8caf9c8cf82c256a9173730a41f3d831dd2e5
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-17 23:55:52 +0200

    Updated Clown.tsx

--- src/components/rooms/room4/Clown.tsx
+++ src/components/rooms/room4/Clown.tsx
@@ -1,13 +1,3 @@
-/* import DoorButton from "@/components/shared/DoorTransition";
-
-export default function Clown() {
-  return (
-    <div className="absolute inset-0 bg-[url('/assets/images/clown.png')] bg-cover">
-      <DoorButton buttonText="Exit house" />
-    </div>
-  );
-} */
-
 "use client";
 
 import { useState, useEffect } from "react";
